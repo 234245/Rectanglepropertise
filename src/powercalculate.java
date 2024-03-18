ddff777b9814c127305bc29ffc5718c4c3ff7ec1
hafsa234245 <hafsaakter7534@gmail.com>
2024-03-18 20:08:05 +0600

    square,cube,fourth powercalculate solved by java.

---
+++ src/powercalculate.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class powercalculate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+        double square = number * number;
+        double cube = Math.pow(number, 3);
+        double fourthPower = Math.pow(number, 4);
+        System.out.println("Square: " + square);
+        System.out.println("Cube: " + cube);
+        System.out.println("Fourth power: " + fourthPower);
+
+        scanner.close();
+    }
+}
